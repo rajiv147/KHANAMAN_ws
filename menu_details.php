1ef8b3dc8e01dfffea4d783180aa6a8c1bef86d9
rajiv147 <rajeevroshan19@gmail.com>
2015-09-19 18:16:23 +0530

    all file related to menu pages
    in these file info and reviews pages added

---
+++ menu_details.php
@@ -0,0 +1,326 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<link href="ddmenu/ddmenu.css" rel="stylesheet" type="text/css"/>
+<script src="ddmenu/ddmenu.js" type="text/javascript"></script>
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>khanaman</title>
+<link rel="stylesheet" href="assets/demo.css">
+<link rel="stylesheet" href="assets/header-login-signup.css">
+<link href='http://fonts.googleapis.com/css?family=Cookie' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Bree+Serif' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Philosopher' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="css/bootstrap.min.css"/>
+<link rel="stylesheet" href="css/font-awesome.min.css"/>
+<link rel="stylesheet" href="css/style.css" />
+<script src="js/vendor/modernizr-2.6.2.min.js"></script>
+<script src="js/vendor/jquery-1.10.2.min.js"></script>
+<script src="js/vendor/bootstrap.min.js"></script>
+<link rel="stylesheet" type="text/css" href="menu_items_info.css">
+
+
+<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">-->
+
+
+
+<header class="header-login-signup">
+<div class="header-limiter">
+<div class="col-sm-8">
+<a href="index.php">
+<img src="images/logo.png" class="responsive-image"></a>
+</div>
+
+
+
+<div class="col-sm-">
+<div class="container-fluid">
+<div class="row">
+
+
+<script type="text/javascript">
+function slideonlyone(thechosenone) {
+     $('.newboxes2').each(function(index) {
+          if ($(this).attr("id") == thechosenone) {
+               $(this).slideDown(200);
+          }
+          else {
+               $(this).slideUp(600);
+          }
+     });
+}
+
+ </script>
+    
+    
+</head>
+<body>
+<div class="content_menu">
+
+
+  
+        
+
+          
+          <a id="myHeader1" href="javascript:slideonlyone('newboxes1');">
+            <i class="fa fa-bars"></i>&nbsp;&nbsp;&nbsp;Menu</a>
+         <a id="myHeader2" href="javascript:slideonlyone('newboxes2');" >
+          <i class="fa fa-info-circle"></i>&nbsp;&nbsp;&nbsp;Info</a>
+           <a id="myHeader3" href="javascript:slideonlyone('newboxes3');" >
+            <i class="fa fa-star"></i>&nbsp;&nbsp;&nbsp;Review</a>
+       
+         <div class="vb_gap"></div>
+
+
+         <div class="newboxes2" id="newboxes1" style="display: block;padding: 5px; width:100%;">this is the Menu area</div>
+      
+
+
+
+
+    
+
+<!-- strats info area -->
+
+         <div class="newboxes2" id="newboxes2" style="display: none;padding: 5px;width:100%;">this is is the info area
+              <div class="vendor_info">New Ganesh Family Dhaba Info</div>
+              <div class="vendor_cuisines_title"><i class="fa fa-cutlery"></i> Cuisine </div>
+              <div class="vendor_cuisines">Chinese in Hyderabad, Hyderabadi in Hyderabad , North Indian in Hyderabad, Regional in Hyderabad</div>
+
+
+
+
+                        <div class="vendor_schedules_title">
+                          <i class="fa fa-clock-o">&nbsp;&nbsp;&nbsp;<b>Delivery hours</b></i>
+
+
+
+                     
+
+<div class="vendor_overview_list">
+  <div class="vendor_overview_list_inside">
+
+  <i class="fa fa-money">&nbsp;&nbsp;&nbsp;<b>Delivery fee</b></i><br>
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;From Rs.30.00
+</div>
+
+<div class="vendor_overview_list_inside" style="padding-top: 100px;">
+
+  <i class="fa fa-clock-o">&nbsp;&nbsp;&nbsp;<b>Pickup Time</b></i><br>
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;From Rs.30.00
+</div>
+
+<div class="vendor_overview_list_inside"style="padding-top: 100px;">
+
+  <i class="fa fa-clock-o">&nbsp;&nbsp;&nbsp;<b>Delivery time</b></i><br>
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;From Rs.30.00
+</div>
+
+</div>
+                        </div>
+
+
+                        <div class="week_schedules">
+                          <div class="week_days">
+                             Monday <br><br>
+                             Tuesday <br><br>
+                              Wednesday<br> <br>
+                              Thursday <br><br>
+                               Friday <br><br>
+                             Saturday <br><br>
+                                Sunday 
+                          </div>
+
+
+
+                          <div class="time_daily">
+                             11:00 - 22:30<br><br>
+ 11:00 - 22:30<br><br>
+ 11:00 - 22:30<br><br>
+11:00 - 22:30<br><br>
+ 11:00 - 22:30<br><br>
+ 11:00 - 22:30<br><br>
+11:00 - 22:30<br><br>
+                          </div>
+                          
+                                               </div>
+
+<div class="vendor_schedules_title">
+                          <i class="fa fa-clock-o">&nbsp;&nbsp;&nbsp;<b>Takeway hours </b></i>
+
+</div>
+                                                <div class="week_schedules">
+                          <div class="week_days">
+                             Monday <br><br>
+                             Tuesday <br><br>
+                              Wednesday<br> <br>
+                              Thursday <br><br>
+                               Friday <br><br>
+                             Saturday <br><br>
+                                Sunday 
+                          </div>
+
+
+
+                          <div class="time_daily">
+                             11:00 - 22:30<br><br>
+ 11:00 - 22:30<br><br>
+ 11:00 - 22:30<br><br>
+11:00 - 22:30<br><br>
+ 11:00 - 22:30<br><br>
+ 11:00 - 22:30<br><br>
+11:00 - 00:30<br><br>
+                          </div>
+
+
+
+                          
+                                               </div>
+
+
+                                               <div style="clear:both;"></div>
+<div class="address">  <i class="fa fa-map-marker"></i> &nbsp;&nbsp;&nbsp;Address</div> 
+
+<div class="address_details">
+  House No.: 3-10-51,Near Foot Ball Ground,Tirumulgherry 'x' Road, Hyderabad, 500015
+</div>                                           
+
+
+<div style="clear:both;"></div>
+
+<div class="address">  <i class="fa fa-map-marker"></i> &nbsp;&nbsp;&nbsp;Delivery locations</div>
+
+<div class="address_details">
+  
+     Alwal (Secunderabad) ,Karkhana (Secunderabad)
+      Karkhana - Secunderabad Lal Bazar Lal Bazar (Secunderabad), Lal Bazar -
+       Secunderabad Lothkunta (Alwal), New Bowenpally New Bowenpally (Secunderabad)
+        New Bowenpally , Secunderabad Old Alwal (Alwal) Old Bowenpally Old Bowenpally 
+        Old Bowenpally (Secunderabad) Old Bowenpally , Secunderabad Subhash Nagar (Secunderabad) 
+        Subhash Nagar , Secunderabad Trimulgherry (Secunderabad) Trimulgherry , Secunderabad 
+
+
+
+</div>      
+</div>
+
+                   
+
+         </div>
+
+
+      
+
+<!-- ends info area -->
+
+
+      <!-- strats review area -->
+         
+         <div class="newboxes2" id="newboxes3" style="width: 95%;margin-left: 57px;">this is the review area
+
+              <div class="review_main">
+
+                <div class="review_sub">
+
+
+
+                 <div class="review_detail_one"> Reviews of New Ganesh Family Dhaba</div>
+                 <div class="review_date">SEP 13, 2015</div>
+                 <div class="rating_star">
+                  <i class="fa fa-star"></i> 
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star-half-o"></i>
+                  <i class="fa fa-star-o"></i>&nbsp;&nbsp;&nbsp;by rajiv</div>
+
+
+                  <div class="comment_area">Packing was Horrible... this was reported last time as well then too same thing happened again.</div>
+
+
+
+
+                </div>
+
+
+
+
+              </div>
+
+
+
+               <div class="review_main">
+
+                <div class="review_sub">
+
+
+
+                
+                 <div class="review_date">SEP 12, 2015</div>
+                 <div class="rating_star">
+                  <i class="fa fa-star"></i> 
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star-half-o"></i>
+                  <i class="fa fa-star-o"></i>&nbsp;&nbsp;&nbsp;by vinay</div>
+
+
+                  <div class="comment_area">Excellent food...& quick delivery. Not much masalas used....properly spiced and close to home-made quality!
+                  </div>
+
+
+
+
+                </div>
+
+
+
+
+              </div>
+
+
+
+               <div class="review_main">
+
+                <div class="review_sub">
+
+
+
+                 
+                 <div class="review_date">SEP 10, 2015</div>
+                 <div class="rating_star">
+                  <i class="fa fa-star"></i> 
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                 <i class="fa fa-star"></i>
+                  <i class="fa fa-star-o"></i>&nbsp;&nbsp;&nbsp;by vibha</div>
+
+
+                  <div class="comment_area">Fantastic delivery time, got food in 15min, ;) :),
+
+food taste is amazing, we would like to order more& more
+
+Thanks , and cheers</div>
+
+
+
+
+                </div>
+
+
+
+
+              </div>
+
+
+
+
+         </div>
+    
+<!-- ends info area -->
+
+</div>
+</body>
+</html>
